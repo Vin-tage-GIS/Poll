647aaee24eaa274f5fe538314b607db2f188dc36
nyatado98 <danndong080@gmail.com>
2023-08-28 16:29:02 +0300

    smtp loading error

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -430,8 +430,8 @@ EMAIL_HOST = 'mail.northriftsolutions.com'
 EMAIL_HOST_USER = 'maono@northriftsolutions.com'
 EMAIL_HOST_PASSWORD = 'M@ono2030'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # 
 
